fb01e1958b647ebe5d0100d14e5afe0510343f0a
Emir <ekntsat79@gmail.com>
2019-06-28 21:42:09 +0300

    2nd commint

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,8 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      // require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,13 +26,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    customLaunchers: {
+    /* customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
         flags: ['--headless', '--disable-gpu', '--no-sandbox', '--remote-debugging-port=9222']
       }
-    },
-    browsers: ['ChromeHeadless'],
+    }, */
+    browsers: ['PhantomJS'],
     singleRun: true,
     restartOnFileChange: true
   });
